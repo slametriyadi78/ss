cb7110d55bf017c4425c731dc96fc139590ca92e
slametriyadi78 <73358198+slametriyadi78@users.noreply.github.com>
2020-11-06 02:05:20 +0700

    Add files via upload

---
+++ mine_beam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=beam.sunpool.top:3334
+WALLET=32f2e8765c2e8f5ea41becc5f397024c94d80cc5fc50ee917af23b260ecb3a5f.testRun
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolMiner --coin ETC --pool etc-eu1.nanopool.org:19999 --user 0xba4164ea05ae353687c7c8cd7c06144a3d599010.worker didiriyadi78
